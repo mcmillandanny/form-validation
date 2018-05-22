e13f23b8d299bb4772108d869e3d20db36f83f83
DannyMcMillan <mcmillandanny@gmail.com>
2018-05-22 00:49:07 -0400

    added styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,17 @@
-"use strict";
+'use strict';
 
-var emailInput = document.querySelector("#email_input");
+var emailInput = document.querySelector('#email');
 
-emailInput.addEventListener("keyup", function () {
-    event.preventDefault();
+emailInput.addEventListener('keyup', function () {
     var emailInputValue = emailInput.value;
-    var symbolCheck = emailInputValue.includes("@");
-    var endCheck = emailInputValue.includes(".com");
-    if (emailInputValue == "") {
-        emailInput.removeAttribute("style");
+    var symbolCheck = emailInputValue.includes('@');
+    var endCheck = emailInputValue.includes('.com');
+    if (emailInputValue == '') {
+        emailInput.removeAttribute('style');
     } else if (symbolCheck && endCheck) {
-        emailInput.style.borderBottomColor = "#00e600";
+        emailInput.style.borderBottomColor = '#00e600';
     } else {
-        emailInput.style.borderBottomColor = "#ff0000";
+        emailInput.style.borderBottomColor = '#ff0000';
     }
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,23 @@
 
 
 
-const emailInput = document.querySelector("#email_input")
+let emailInput = document.querySelector('#email')
 
-emailInput.addEventListener("keyup", function(){
-    event.preventDefault();
-    const emailInputValue = emailInput.value
-    const symbolCheck = emailInputValue.includes("@")
-    const endCheck = emailInputValue.includes(".com")
-    if (emailInputValue == ""){
-        emailInput.removeAttribute("style")
+emailInput.addEventListener('keyup', function(){
+    let emailInputValue = emailInput.value
+    let symbolCheck = emailInputValue.includes('@')
+    let endCheck = emailInputValue.includes('.com')
+    if (emailInputValue == ''){
+        emailInput.removeAttribute('style')
     } else if (symbolCheck && endCheck){
-        emailInput.style.borderBottomColor = "#00e600";
+        emailInput.style.borderBottomColor = '#00e600';
     } else {
-        emailInput.style.borderBottomColor = "#ff0000";
+        emailInput.style.borderBottomColor = '#ff0000';
     }
 })
+
+
+
+
+
+
